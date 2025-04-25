e73f01e710b1cd47c341c44a3d6c44a1805a854a
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-04-25 12:04:54 +0300

    Create ExploratoryDataAnalysis.R

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,16 @@
+# Load dataset
+GenderData <- read.csv("data/gender_classification_v7.csv", colClasses = c(
+  long_hair = "factor",
+  forehead_width_cm = "numeric",
+  forehead_height_cm = "numeric",
+  nose_wide = "factor",
+  nose_long = "factor",
+  lips_thin = "factor",
+  distance_nose_to_lip_long = "factor",
+  gender = "factor"
+))
+
+# Preview structure
+str(GenderData)
+head(GenderData)
+View(GenderData)
